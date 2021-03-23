d975e428e0511c14bd4768cd23de3e71f8ac9559
Robert Szyszka <robert.szyszka@outlook.com>
2021-03-23 21:50:40 +0100

    State before Card

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
+import Creator from '../Creator/Creator.js';
 import styles from './List.scss';
 import PropTypes from 'prop-types';
-import {settings} from '../../data/dataStore';
+import {settings} from '../../data/dataStore.js';
 
 class List extends React.Component {
+    state = {
+        columns: this.props.columns || [],
+      }
     static propTypes = {
         title: PropTypes.node.isRequired,
         description: PropTypes.node,
@@ -14,6 +18,23 @@ class List extends React.Component {
     static defaultProps = {
         description: settings.defaultListDescription,
     }
+
+    addColumn(title){
+        this.setState(state => (
+          {
+            columns: [
+              ...state.columns,
+              {
+                key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+                title,
+                icon: 'list-alt',
+                cards: []
+              }
+            ]
+          }
+        ));
+    }
+
     render() {
         return (
         <section className={styles.component}>
@@ -22,10 +43,12 @@ class List extends React.Component {
                 {this.props.description}
             </div>
             <div className={styles.columns}>
-                
-                <Column columnTitle={'Animals'} />
-                <Column columnTitle={'Plants'} />
-                <Column columnTitle={'Minerals'} />
+                {this.state.columns.map(({key, ...columnProps}) => (
+                <Column key={key} {...columnProps} />
+                ))}
+            </div>
+            <div className={styles.creator}>
+                <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
             </div>
         </section>
         )
